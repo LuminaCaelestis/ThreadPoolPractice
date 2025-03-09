fa53c01194155c5d8aac84c333295a9a6feab8ec
Mute <LuminaCaelestis@users.noreply.github.com>
2025-03-09 22:50:28 +0800

    ThreadPool.cs - 静态构造

--- Program.cs
+++ Program.cs
@@ -8,25 +8,13 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            Flos.Threading.ThreadPool threadPool = new(3);
-
-            threadPool.Start();
-
-            for (int i = 0; i < 15; i++)
-            {
-                threadPool.AddTask(() => HeavyJob());
-            }
-            
-            Thread.Sleep(6000);
-            threadPool.Shutdown();
-
         }
 
 
         public static void HeavyJob()
         {
             Thread.Sleep(1000);
-            Console.WriteLine($"{Environment.CurrentManagedThreadId} - Heavy Job Done");
+            Console.WriteLine($"{Environment.CurrentManagedThreadId} - Done");
         }
     }
 }
--- ThreadPool.cs
+++ ThreadPool.cs
@@ -4,101 +4,52 @@ using System.Threading;
 
 namespace Flos.Threading
 {
-    public class ThreadPool
+    public static class ThreadPool
     {
-        public int MaxThreadCount { get; set; }
+        public static int MaxThreadCount { get; set; }
 
-        private int _runningThreadCount;
+        private static int _runningThreadCount;
 
-        public int RunningThreadCount =>  _runningThreadCount;
+        public static int _alivedThreadCount;
 
-        private bool _shutdown = false;
+        private static bool _shutdown = false;
 
-        private readonly List<Thread> _threadList = new();
+        private static readonly List<Thread> _coreThreadList = new();
 
-        private readonly Container.BlockingQueue<Action> _taskQueue = new();
+        private static readonly Container.BlockingQueue<Action> _taskQueue = new();
 
-        // 禁用拷贝构造
-        private ThreadPool(ThreadPool other)
+        static ThreadPool()
         {
-            throw new NotSupportedException("Copy constructor is not supported");
-        }
-
-        // 默认构造
-        public ThreadPool() : this(Environment.ProcessorCount)
-        {
+            _alivedThreadCount = 0;
             _runningThreadCount = 0;
-            MaxThreadCount = Environment.ProcessorCount;
-        }
+            MaxThreadCount = Environment.ProcessorCount * 2;
+            var CoreThreadCount = Environment.ProcessorCount;
 
-        public ThreadPool(int numCoreThread)
-        {
-            if(numCoreThread < 1)
+            for(int i = 0; i < CoreThreadCount; ++i)
             {
-                throw new ArgumentException("AvalibleThreadCount must be greater than 0");
-            }
-            _idleThreadCount = numCoreThread;
-            MaxThreadCount = numCoreThread;
-        }
-
-        public void Shutdown()
-        {
-            _shutdown = true;
-            _workQueue.PulseAll();
-            foreach (Thread thread in _threads)
-            {
-                thread.Join();
-            }
-            _threads.Clear();
-        }
-
-        public bool AddTask(Action task)
-        {
-            if (task == null)
-            {
-                throw new ArgumentNullException(nameof(task));
-            }
-            if (_shutdown)
-            {
-                throw new InvalidOperationException("ThreadPool is shutting down");
-            }
-            return _workQueue.TryEnqueue(task);
-        }
-
-        private void Worker()
-        {
-            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} started");
-            // 工作线程
-            while (!_shutdown)
-            {
-                try
-                {
-                    Interlocked.Decrement(ref _idleThreadCount);
-                    Action method = _workQueue.Dequeue();
-                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Invoke {method.Method.Name}");
-                    method.Invoke();
-                }
-                catch (Exception e)
-                {
-                    throw new Exception("An error occurred in a thread", e);
-                }
-                finally
-                {
-                    Interlocked.Increment(ref _idleThreadCount);
-                }
-            }
-            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} stopped");
-        }
-
-        public void Start()
-        {
-            Interlocked.Exchange(ref _shutdown, false);
-            for (int i = 0; i < _idleThreadCount; ++i)
-            {
-                Thread thread = new Thread(Worker);
-                _threads.Add(thread);
+                var thread = new Thread(CoreThread);
+                _coreThreadList.Add(thread);
                 thread.Start();
+                Interlocked.Increment(ref _alivedThreadCount);
             }
         }
+
+        private static void CoreThread()
+        {
+            while(!_shutdown)
+            {
+                var isPopped = _taskQueue.TryPop(out var task);
+                if(!isPopped)
+                {
+                    continue;
+                }
+                task?.Invoke();
+            }
+        }
+
+        public static void WorkerThread()
+        {
+        }
+
     }
 }
